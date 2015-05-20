55238b6d9977e2a762b3a43b7bb2a0bdb0d12892
kellycjq <jwchen@l-jwchen00.local>
2015-05-19 20:59:24 -0700

    data viz for joyce
    data viz for joyce

---
+++ data.js
@@ -0,0 +1,18 @@
+var data0= [null, null, null, null, null, null, null, null];
+
+
+var data1=  [13,null,null,13.63, 16.53,28.81,34.70, 31.23 ];			
+var data2= [null, null, 9.11,14.35, 21.86, 36.18,39.47,null];
+var data3=  [null, 7.03, 8.68, 20.63, 30.45, 37.15, null,null];
+var data4=  [5.63, 5.77, 12.46, 27.66, 36.39, null, null, null];
+
+			var data5=   [5.82, 9.60, 23.63, 29.60, null, null, null, null];
+					   
+			var data6= [14.14, 15.10, 23.56,null, null, null, null, null];
+			var data7=  [15.76, 18.39 , null, null, null, null, null, null];
+			
+			var data8=  [12.00, null, null, null, null, null, null, null];
+
+			
+				
+	
